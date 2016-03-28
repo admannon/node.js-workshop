a99b99b73f64cd5223fdcd37ddf94da42cce6487
Peerakarn Junmunee <admannon@hotmail.com>
2016-03-28 19:09:12 +0700

    Update optional

--- node_express.js
+++ node_express.js
@@ -9,22 +9,20 @@ app.get("/hello", function (request, response, next) {
 // Show static webpage
 app.use(express.static("./asserts"));
 
+// Redirection
+app.use('/the-fox', function (request, response, next) {
+    response.redirect("/arhi.jpg");
+});
+
+// Download
+app.use('/download-fox', function (request, response, next) {
+    response.download(__dirname + '/asserts/arhi.jpg');
+});
+
 // Status: 404
 app.use(function (request, response, next) {
     // IE not show anything after status(404)
     response.status(404).end("Not found.");
 });
-
-/* // Redirection
-app.use(function (request, response, next) {
-    response.redirect("/");
-});
-*/
-
-/* // Download
-app.use(function (request, response, next) {
-    response.download();
-});
-*/
 
 app.listen(80);
