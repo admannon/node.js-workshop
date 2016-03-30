160820bf8f02d08d4871cfd79d35fd4edcc6a411
Peerakarn Junmunee <admannon@hotmail.com>
2016-03-30 19:20:18 +0700

    Add routing

--- node_express.js
+++ node_express.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app     = express();
 
+// Routing system
+var router  = express.Router();
+
+// Usage: router.ANYFUNC ~
+// app.use(ANYPATH, router)
+
+router.use(function (request, response, next) {
+    response.send(`${request.url.substr(1)} bird`);
+});
+
+app.use("/bird", router);
+
 app.get("/hello", function (request, response, next) {
     // next() for next operation.
     response.end("World");
@@ -9,20 +21,21 @@ app.get("/hello", function (request, response, next) {
 // Show static webpage
 app.use(express.static("./asserts"));
 
-// Redirection
+// redirect ( location )
 app.use('/the-fox', function (request, response, next) {
     response.redirect("/arhi.jpg");
 });
 
-// Download
+// download ( file_location [, name] );
 app.use('/download-fox', function (request, response, next) {
     response.download(__dirname + '/asserts/arhi.jpg');
 });
 
-// Status: 404
+// status ( status_number )
 app.use(function (request, response, next) {
     // IE not show anything after status(404)
     response.status(404).end("Not found.");
+    // response.sendFile("File")
 });
 
 app.listen(80);
